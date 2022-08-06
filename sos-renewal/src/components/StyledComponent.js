004ad879a105ee201b8401a5927abd815e92b3e5
jihyun517 <50868983+jihyun517@users.noreply.github.com>
2022-08-06 14:47:04 +0900

    MenuBar 탬플릿 완료(Activated)

--- sos-renewal/src/components/StyledComponent.js
+++ sos-renewal/src/components/StyledComponent.js
@@ -47,3 +47,74 @@ export const HeaderSearchIcon = styled(HiOutlineSearch)`
   margin-right: 10px;
   color: gray;
 `;
+
+//////////////////// MenuBar ////////////////////
+export const MenuBarWrapper = styled.div`
+  position: absolute;
+  width: 100%;
+  top: 80px;
+`;
+
+export const MenuBarTitleWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 55px;
+  background: #08517A;
+  display: flex;
+  justify-content: center;
+`;
+
+export const MenuBarTitle = styled.div`
+  width: 250px;
+  height: 55px;
+  background: #08517A;
+  font-size: 23px;
+  font-weight: 300;
+  color: white;
+  display : flex;
+  justify-content : center;
+  align-items : center;
+  
+`
+export const MenuBarActivatedWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+
+  background-color: white;
+  //background-color: lightgray;
+`
+export const MenuBarActivatedBox = styled.div`
+  width: 250px;
+  height: 300px;
+  padding: 10px 0;
+  display: flex;
+  flex-direction: column;
+
+  background-color: white;
+  //background-color: pink;
+`
+export const MenuBarActivatedDetail = styled.div`
+  width: 100%;
+  height: 50px;
+  margin-left: 60px;
+  display: flex;
+  align-items: center;
+
+  font-size: 15px;
+  font-weight: 100;
+  color: #6C6C6C;
+
+  background-color: white;
+`
+
+export const MenuBarActivatedVertical = styled.div`
+  height: 280px;
+  width: 1px;
+  margin-top: 20px;
+  background-color: #CECECE;
+
+`
+
+
